843910a56e43f64a626f2254749a4e2de3e0ad65
Pangraf <emmanuelzoubinnaba@gmail.com>
2018-12-29 14:48:45 +0000

    Personnalisation de mon site, la partie static

---
+++ resources/views/layouts/partials/_nav.blade.php
@@ -0,0 +1,39 @@
+
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Laracarte</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Artisans</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Planet <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="http://laravel.com">Laravel.com</a></li>
+                <li><a href="http://laravel.io">Laravel.io</a></li>
+                <li><a href="http://laracasts.com">Laracasts</a></li>
+                <li><a href="http://larajobs.com">Larajobs</a></li>
+                <li><a href="http://laravel-news.com">Laravel News</a></li>
+                <li><a href="http://larachat.com">Larachat</a></li>
+              
+              </ul>
+            </li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#about">Login</a></li>
+            <li><a href="#contact">Register</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+</nav>
