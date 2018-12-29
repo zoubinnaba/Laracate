cdba11db460b63c8a347fe88b016e1bba5fd8137
Pangraf <emmanuelzoubinnaba@gmail.com>
2018-12-29 16:42:38 +0000

    Ajout d'un helper set_active_route

--- app/helpers.php
+++ app/helpers.php
@@ -3,12 +3,18 @@
 if(! function_exists('page_title')) {
 	function page_title($title) {
 		$base_title = 'Laracate - Liste of artisans';
-		
+
 		if($title == '') {
 			return $base_title;
 		} else {
 			return $title . ' | ' . $base_title;
 		}
 
+	}
+}
+
+if(! function_exists('set_active_route')) {
+	function set_active_route($route) {
+		return Route::is($route) ? 'active' : '';
 	}
 }
